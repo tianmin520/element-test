9be87dd579409978b39a352a63f840cfd6d65c29
git_tianmin <854777283@qq.com>
2020-12-14 09:32:47 +0800

    完成导航条

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -16,14 +16,99 @@
       </el-row>
     </el-header>
     <el-container>
-      <el-aside width="200px" class="Aside">Aside</el-aside>
+      <el-aside width="200px" class="Aside">
+        <el-menu
+      default-active="2"
+      unique-opened="true"
+      active-text-color="#f57555"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+          <el-menu-item index="1-2">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+        </el-menu-item-group>
+       
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航二</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+          <el-menu-item index="2-2">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航三</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+          <el-menu-item index="3-2">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航四</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+          <el-menu-item index="4-2">
+            <i class="el-icon-s-order"></i>
+            <span>分组一</span>
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      
+      
+    </el-menu>
+    </el-aside>
       <el-main class="Main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+};
 </script>
 
 <style>
